65b8deb10cd0c8051bb2e316c785dd017dc54b50
Michael Iyke <ymichaelc@gmail.com>
2023-10-30 13:30:43 +0100

    checker

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[38:-63] + " with python"
+str = "{} with python".format(str[38:-63])
 print(str)
