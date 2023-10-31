853364f3275cc37f15d606d41c154ed8626e51ab
Michael Iyke <ymichaelc@gmail.com>
2023-10-31 17:16:42 +0100

    correct

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for i in range(10):
     for j in range(1, 10):
         if (i != j and i < j):
             if i == 8 and j == 9:
-                print(f"{i}{j}")
+                print("{i}{j}".format(i=i, j=j))
             else:
-                print(f"{i}{j}", end=",")
+                print("{i}{j}".format(i=i, j=j), end=",")
