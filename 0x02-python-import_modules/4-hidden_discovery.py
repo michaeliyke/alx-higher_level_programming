67081439b44d6156b44ee5bd858a9c9cb9610b1b
Michael Iyke <ymichaelc@gmail.com>
2023-11-03 00:54:54 +0000

    corrrect

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 import hidden_4 as h
 
 arr = sorted(dir(h))
 if __name__ == "__main__":
-  for e in arr:
-    if not e.startswith("__"):
-      print("{}".format(e))
+    for e in arr:
+        if not e.startswith("__"):
+            print("{}".format(e))
