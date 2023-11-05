e95152cf73a2ef6f86f1dfce12c28fc2b9ed9c30
Michael Iyke <ymichaelc@gmail.com>
2023-11-05 13:33:46 +0000

    correct

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,5 +5,5 @@ def print_list_integer(my_list=[]):
     if not my_list:
         return
     for i in my_list:
-        if type(i) == int:
+        if isinstance(i, int):
             print("{0}".format(i))
