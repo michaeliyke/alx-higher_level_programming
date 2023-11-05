fced281d8c0dc71a4c2642168ab3881a23b4db5c
Michael Iyke <ymichaelc@gmail.com>
2023-11-05 18:34:56 +0000

    correct

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@
 def new_in_list(my_list, idx, element):
     if not my_list:
         return my_list
-    cpy = my_list.copy()
+    cpy = [i for i in my_list]
     if not isinstance(idx, int):
         return cpy
     if idx < 0 or idx >= len(my_list):
