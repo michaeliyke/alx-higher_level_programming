6ad1c2cca79a492112f9232a2dcc00dd4109a3d1
Michael Iyke <ymichaelc@gmail.com>
2023-11-05 19:22:30 +0000

    correct to int printing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
             c = len(row)
-            print("{}".format(col), end="")
+            print("{:d}".format(col), end="")
             if col != row[len(row) - 1]:
                 print(end=" ")
         print()
