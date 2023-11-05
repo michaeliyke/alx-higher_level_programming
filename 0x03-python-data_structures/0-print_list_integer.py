72339cb95a822e0a4bfbe18637df0cb264343837
Michael Iyke <ymichaelc@gmail.com>
2023-11-05 15:16:57 +0000

    correct

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,5 +5,4 @@ def print_list_integer(my_list=[]):
     if not my_list:
         return
     for i in my_list:
-        if isinstance(i, int):
-            print("{:d}".format(i))  # print as a number without converting
+        print("{:d}".format(i))  # print as a number without converting
