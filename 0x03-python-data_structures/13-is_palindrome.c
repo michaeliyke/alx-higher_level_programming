7c63864d8403ccd15f330df27fbcd08ea02b0aef
Michael Iyke <ymichaelc@gmail.com>
2023-11-06 13:26:31 +0000

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,29 +5,35 @@
  * @h: the head of the list
  *
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- * @NOTE:
- * 1. use fast-slow steps get the middle and the end.
+ * NOTE:
+ * 1. use fast-slow steps to get the middle and the end.
  * 2. reverse the 2nd half of the list in place,
  * this splits the list in two halfs with the 2nd half reversed
  * 3. check if the elements of both match
+ *
+ * REVERSE THE SECOND HALF
+ * STEPS:
+ * 1. save the next spot in a variable
+ * 2. put both slow and temp on the next of that variable
+ * LOOP:
+ * Advance slow to the next of slow and assign the next of temp to that vriable
+ * assign that variable to temp, assign temp to slow
  */
 int is_palindrome(listint_t **h)
 {
-	listint_t *fast, *slow, *nh;
+	listint_t *fast, *slow, *hh, *old;
 
-	if (h == NULL || *h == NULL)
+	if (h == NULL || *h == NULL) /* If no elemnts */
 		return (1);
-	fast = *h;
+
+	if (!((*h)->next)) /* if only one element */
+		return (1);
+	if (!((*h)->next->next)) /* if only two elements */
+		return ((*h)->next->n == (*h)->n);
+
 	slow = *h;
-	if (!(fast = fast->next)) /* if only one element */
-		return (1);
-	if (!(fast = fast->next)) /* if there are only two elements */
-		return (fast->n == slow->n);
-
-	/**
-	 * there's at least three elements
-	 * drive fast to the end and slow to almost the middle
-	 */
+	fast = (*h)->next->next;
+	/* At least 3 elems, drive fast to the end and slow to the middle */
 	while (fast && fast->next)
 	{
 		slow = slow->next;
@@ -36,24 +42,21 @@ int is_palindrome(listint_t **h)
 			fast = fast->next;
 	}
 
-	/* Reverse the second half */
-	nh = new_listint();
+	old = slow; /* end of first half */
+	hh = fast->next == NULL ? slow : slow->next;
+	slow = fast = slow->next;
 
-	while (slow)
+	old->next = NULL;
+	hh->next = NULL;
+	while (fast)
 	{
+		slow = slow->next;
+		fast->next = hh;
+		hh = fast;
+		fast = slow;
 	}
-}
-
-/**
- * new_listint - make a new listint linked list
- *
- * Return: pointer to a new listint linked list
- */
-listint_t *new_listint(void)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
+	for (old = *h; old && hh; old = old->next, hh = hh->next)
+		if (old->n != hh->n)
+			return (0);
+	return (1);
 }
