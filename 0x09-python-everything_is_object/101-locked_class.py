52ec23d3606d76e3920cadfd02881ea642b51d06
Michael Iyke <ymichaelc@gmail.com>
2023-11-29 01:28:47 +0100

    LockedClass with __slots__ attribute

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -10,18 +10,4 @@ class LockedClass:
     """Class with no instnce attributes
     """
 
-    def __setattr__(self, name, value):
-        """Prohibits setting of instace attributes except first_name
-
-        Args:
-            name (str): an attribute about to be set on on instance
-            value (Any): The value to set the attribute to
-
-        Raises:
-            AttributeError: raises for all attribute setting except first_name
-        """
-        if name == "first_name":
-            super().__setattr__(name, value)
-        else:
-            text = "'{}' object has no attribute '{}'"
-            raise AttributeError(text.format("LockedClass", name))
+    __slots__ = ("first_name")
