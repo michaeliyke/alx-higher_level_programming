05d5f2fda643a21e8beb0c60512abf8dbf6a6ebe
Michael Iyke <ymichaelc@gmail.com>
2023-12-06 13:03:37 +0100

    Class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin
+"""Module that returns dictionary discription of an object"""
+
+
+def class_to_json(obj):
+    """Returns the dictionary description with simple data structure
+     for JSON serialization of an object"""
+    if type(obj) in (dict, str, list, int, bool):
+        return obj.__dict__
+    elif isinstance(obj, object):
+        return obj.__dict__
--- 0x0B-python-input_output/main.py
+++ 0x0B-python-input_output/main.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python3
-append_write = __import__('2-append_write').append_write
+""" My class module
+"""
+class_to_json = __import__('8-class_to_json').class_to_json
 
-nb_characters_added = append_write(
-    "file_append.txt", "This School is so cool!\n")
-print(nb_characters_added)
+
+class MyClass:
+    """ My class
+    """
+
+    def __init__(self, name):
+        self.name = name
+        self.number = 0
+
+    def __str__(self):
+        return "[MyClass] {} - {:d}".format(self.name, self.number)
+
+
+m = MyClass("John")
+m.number = 89
+print(type(m))
+print(m)
+
+mj = class_to_json(m)
+print(type(mj))
+print(mj)
