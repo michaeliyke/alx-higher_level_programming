cf5ec44aa9dfcf258f0932592d6d21c71a7f8ad1
Michael Iyke <ymichaelc@gmail.com>
2023-12-06 14:10:33 +0100

    Student to Json

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Student class module"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instance of the Student class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
