c0c6775ecb4b0d45fce3eb2e003a460eaa343988
Michael Iyke <ymichaelc@gmail.com>
2023-12-08 18:37:24 +0100

    And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""The square module"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """The sqaure class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """The square constructor"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width
+        )
