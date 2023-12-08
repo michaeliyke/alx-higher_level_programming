11d9704eee751e2aa7293a49a1d64fcc75007761
Michael Iyke <ymichaelc@gmail.com>
2023-12-08 18:55:49 +0100

    Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,6 +11,19 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """String representation of the square object"""
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width
         )
+
+    @property
+    def size(self):
+        """Getting the size of the square"""
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        """Setting the size of the square"""
+        self.validate("width", size)
+        self.width = size
+        self.height = size
