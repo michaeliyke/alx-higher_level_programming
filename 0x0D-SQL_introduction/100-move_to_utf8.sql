a5e31be101b9a388e6096a030e391e9b591bbe76
Michael Iyke <ymichaelc@gmail.com>
2024-01-17 19:44:05 +0100

    17. Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
--- Chanage the character set of the db
+-- change the character set of the database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- change the character set of the table
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
