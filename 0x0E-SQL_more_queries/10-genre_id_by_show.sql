1027d32ff396564c8c8090fedec0f4cf970e3f84
Michael Iyke <ymichaelc@gmail.com>
2024-01-19 23:11:50 +0100

    cls

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- list all shows contained in hbtn_0d_tvshows that have at least one genre linked
+SELECT
+	title, genre_id
+FROM
+	tv_shows
+JOIN
+	tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+ORDER BY title, genre_id ASC;
