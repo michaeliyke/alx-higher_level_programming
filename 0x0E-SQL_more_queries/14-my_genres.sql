72a034f65687ee8209311b82d1bef5964e7af5fa
Michael Iyke <ymichaelc@gmail.com>
2024-01-20 06:01:34 +0100

    14. My genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,12 +1,13 @@
 -- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+-- uses a databse to lists all rows in a table corresponding to all rows in another
 SELECT 
-	genres.name AS name, shows.title AS title
+	genres.name AS name
 FROM
 tv_shows AS shows
 INNER JOIN
 	tv_show_genres AS show_genres ON shows.id = show_genres.show_id
 INNER JOIN
-	tv_genres AS genres
+	tv_genres AS genres ON genres.id = show_genres.genre_id
 WHERE
 	shows.title = "Dexter"
 ORDER BY name ASC;
