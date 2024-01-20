c8570d094ee9fac94f8f520b7e5580cd74569664
Michael Iyke <ymichaelc@gmail.com>
2024-01-20 04:03:11 +0100

    13. Number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,15 +2,15 @@
 SELECT
 	genres.name AS genre,
 	COUNT(shows.title) AS number_of_shows
-FROM
+FROM -- S_ELEC_Ts all of tv_shows for checking
 	tv_shows AS shows
-INNER JOIN
+INNER JOIN -- J_OI_Ns result with the tv_show_genres to obtain genre_id (a foreign key)
 	tv_show_genres AS show_genres ON show_genres.show_id = shows.id
-INNER JOIN
+INNER JOIN -- J_OI_Ns result with tv_genres using the newly obtined genre_id
 	tv_genres AS genres ON genres.id = show_genres.genre_id
-GROUP BY
+GROUP BY -- G_ROU_P by one of the non-agregated coluns
 	genre
-ORDER BY
+ORDER BY -- O_DE_R as directed
 	number_of_shows DESC;
 
 /*
