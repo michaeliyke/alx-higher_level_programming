38a9ccd1f330e985c6ea70ea30e382061ab8505a
Michael Iyke <ymichaelc@gmail.com>
2024-01-20 19:12:28 +0100

    17. Not my genre

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -13,7 +13,7 @@ WHERE
 		FROM
 			tv_genres AS genres
 		LEFT JOIN
-			tv_show_genres as show_genres ON genres.id = show_genres.genre_id
+			tv_show_genres AS show_genres ON genres.id = show_genres.genre_id
 		LEFT JOIN
 			tv_shows AS shows ON shows.id = show_genres.show_id
 		WHERE
