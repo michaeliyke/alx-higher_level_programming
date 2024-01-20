95d6210ca707b8a819d1141369647f242cbdcbf9
Michael Iyke <ymichaelc@gmail.com>
2024-01-20 22:33:24 +0100

    18. No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,1 +1,17 @@
 -- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+SELECT
+	title
+FROM
+	tv_shows AS shows
+LEFT JOIN
+	tv_show_genres AS show_genres ON shows.id = show_genres.genre_id
+WHERE
+	show_genres.genre_id != (
+		SELECT
+			id
+		FROM
+			tv_genres AS genres
+		WHERE genres.name = "Comedy"
+	)
+GROUP BY title
+ORDER BY title ASC;
