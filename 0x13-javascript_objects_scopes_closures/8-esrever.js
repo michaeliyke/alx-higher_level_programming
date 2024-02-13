6905f0b6645e748fa0447d401974e63ec96995f9
Michael Iyke <ymichaelc@gmail.com>
2024-02-13 13:23:01 +0100

    function that returns the reversed version of a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   if (!Array.isArray(list)) return;
-  let len = list.length;
+  const len = list.length;
 
   for (let left = 0, right = len - 1; left < right; left++, right--) {
     [list[left], list[right]] = [list[right], list[left]];
