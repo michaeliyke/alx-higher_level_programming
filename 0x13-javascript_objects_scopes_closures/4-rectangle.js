ceb651bef3032c09a4ac57c313f6731da441fd63
Michael Iyke <ymichaelc@gmail.com>
2024-02-13 14:21:43 +0100

    class Rectangle that defines a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (isNaN(w) || isNaN(h)) return;
+    if (w < 1 || h < 1) return;
+
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let width = '';
+
+      for (let j = 0; j < this.width; j++) {
+        width += 'X';
+      }
+
+      console.log(width);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
+  }
+};
