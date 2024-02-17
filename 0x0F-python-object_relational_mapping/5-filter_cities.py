ac57bf066c9c998358119d70e73246f2ebb765f6
Michael Iyke <ymichaelc@gmail.com>
2024-02-18 00:51:41 +0100

    script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""State Selecting module"""
+# ./5-filter_cities.py michael aka hbtn_0e_4_usa Texas
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv as args
+
+    state = args[4]
+    details = dict(
+        host="localhost", port=3306, user=args[1],
+        passwd=args[2], db=args[3], charset="utf8",
+    )
+
+    conn = MySQLdb.connect(**details)
+    cur = conn.cursor()
+
+    query = """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE BINARY states.name = %s
+        ORDER BY cities.id ASC
+        """
+    cur.execute(query, (state,))
+    query_rows = list(cur.fetchall())
+    i = 0
+    size = len(query_rows)
+    for row in query_rows:
+        if row and i < size - 1:
+            print(row[0], end=", ")
+        else:
+            print(row[0])
+        i += 1
+
+    cur.close()
+    conn.close()
