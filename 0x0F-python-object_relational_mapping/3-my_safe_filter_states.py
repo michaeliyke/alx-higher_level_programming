bc952681f61f6f5ced5a410c6a7fec405726eefb
Michael Iyke <ymichaelc@gmail.com>
2024-02-17 20:35:54 +0100

    script should take 4 arguments: mysql username, mysql password, database name and state name searched (safe from MySQL injection)

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""State Selecting module"""
+# ./3-my_safe_filter_states.py michael aka hbtn_0e_0_usa 'Arizona'
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv as args
+
+    user = args[1]
+    password = args[2]
+    db = args[3]
+    name = args[4]
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=user,
+                           passwd=password, db=db, charset="utf8")
+    cur = conn.cursor()
+
+    query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC"
+    cur.execute(query, (name,))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row:
+            print(row)
+    cur.close()
+    conn.close()
