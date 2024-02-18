a4a5d5b5b403430e5c26db3af09269c98d621cc0
Michael Iyke <ymichaelc@gmail.com>
2024-02-18 02:04:15 +0100

    Implement base model class shifting to sqlachemy

---
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+# ./6-model_state.py michael aka hbtn_0e_6_usa
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
