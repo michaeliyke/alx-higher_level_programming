0538d335a2265711835a590afbbd13c1628afb61
Michael Iyke <ymichaelc@gmail.com>
2024-02-18 02:04:15 +0100

    Implement base model class shifting to sqlachemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
 """Model state module"""
+import sys
 from sqlalchemy import Integer, String, Column
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import (create_engine)
 
 Base = declarative_base()
 
@@ -11,3 +14,10 @@ class State(Base):
     id = Column(Integer, primary_key=True,
                 autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
