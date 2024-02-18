0cef9d5c2ab8d76e367a256e92815c9e95e218b8
Michael Iyke <ymichaelc@gmail.com>
2024-02-18 02:04:15 +0100

    Implement base model class shifting to sqlachemy

--- 0x0F-python-object_relational_mapping/test_orl.py
+++ 0x0F-python-object_relational_mapping/test_orl.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import State, Base
+
 engine = create_engine(
-    'mysql+mysqldb://{}:{}@localhost/{}'.format("root", "root", "my_db"), pool_pre_ping=True)
+    'mysql+mysqldb://{}:{}@localhost/{}'
+    .format("root", "root", "my_db"), pool_pre_ping=True)
 Base.metadata.create_all(engine)
 
 session = Session(engine)
-# HERE: no SQL query, only objects!
 for state in session.query(State).order_by(State.id).all():
     print("{}: {}".format(state.id, state.name))
 session.close()
