64f478c5df1941a94381c4df9de09413059f01a6
Michael Iyke <ymichaelc@gmail.com>
2024-02-29 23:43:05 +0100

    correction

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a URL, and displays the size of the body of the response
-curl -Is -X OPTIONS google.com | grep 'Allow:' | cut -d ':' -f2 | sed 's/^[[:space:]]*//'
+curl -Is -X OPTIONS "$1" | grep 'Allow:' | cut -d ':' -f2 | sed 's/^[[:space:]]*//'
