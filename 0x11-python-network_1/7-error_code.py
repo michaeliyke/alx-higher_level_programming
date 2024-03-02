ae97ecfc1cb2978e4d61055815f1ba93b6a2fdae
Michael Iyke <ymichaelc@gmail.com>
2024-03-02 07:55:43 +0100

    correction

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL and an email, sends a POST request to the
-passed URL with the email as a parameter, and displays the body of the
-response (decoded in utf-8)
+Python script that takes URL sends a request to the URL and displays the body
+of the response
 """
 
 if __name__ == "__main__":
     import requests
     import sys
 
-    res = requests.post(sys.argv[1])
+    res = requests.get(sys.argv[1])
     if res.status_code >= 400:
         print("Error code: ".format(res.status_code))
     else:
