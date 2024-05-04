c8c64ea297565cdcb832e2ed92d4aab2945fba75
Michael Iyke <ymichaelc@gmail.com>
2024-05-04 14:59:48 +0100

    Fetch hello on cick and on enter keypress

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,25 @@
+const jQuery = $;
+
+jQuery(function ($) {
+
+  $('INPUT#btn_translate').click(translate);
+
+  $(document).keypress((event) => {
+    if (event.which === 13) {
+      $('INPUT#btn_translate').trigger('click');
+    }
+  });
+
+});
+
+
+function translate() {
+  let lang = $('INPUT#language_code').val();
+  if (lang === '') {
+    lang = 'en-US';
+  }
+  const url = `https://hellosalut.stefanbohacek.dev/?lang=${lang}`;
+  $.get(url, ({ hello }) => {
+    $('DIV#hello').text(hello);
+  });
+}
