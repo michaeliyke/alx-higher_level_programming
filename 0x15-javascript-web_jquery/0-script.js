32dbe0118473722e05cdbb12bffb94923286da5f
Michael Iyke <ymichaelc@gmail.com>
2024-05-04 10:54:13 +0100

    Change text color of the header tag with jQuery

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,4 +1,4 @@
-; (function (win, sapi) {
+(function (win, sapi) {
   // win and sapi are the window and document objects respectively
   const header = sapi.querySelector('header');
   header.style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+const jQuery = $;
+
+jQuery(function ($) {
+  $('header').css('color', '#FF0000');
+});
