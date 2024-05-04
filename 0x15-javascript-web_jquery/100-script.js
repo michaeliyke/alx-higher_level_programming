8876834c0b753ba2861ae1e47556b9839250cde5
Michael Iyke <ymichaelc@gmail.com>
2024-05-04 12:53:48 +0100

    task 100: import script in head, update header color

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const select = document.querySelector.bind(document);
+  const header = select('header');
+  header.style.color = '#FF0000';
+});
